fa0293feab9160a1fb190e0d24c179a9b091c1a0
Abdul Ghaffar <agscontact777@gmail.com>
2026-02-05 11:06:30 +0500

    Added path prefix like versioning the apis

--- backend-basics/routing-middleware/mux-routing/main.go
+++ backend-basics/routing-middleware/mux-routing/main.go
@@ -16,10 +16,14 @@ func getUserById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["id"]
 	json.NewEncoder(w).Encode(struct {
-		Id string		`json:"id"`
-		Name string		`json:"name"`
-		Age  int		`json:"age"`
+		Id   string `json:"id"`
+		Name string `json:"name"`
+		Age  int    `json:"age"`
 	}{id, "Abdul ghaffar", 22})
+}
+
+func infoOfV1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the version 1 of the apis")
 }
 
 func main() {
@@ -27,5 +31,9 @@ func main() {
 
 	r.HandleFunc("/", homeHandler).Methods("GET")
 	r.HandleFunc("/user/{id}", getUserById).Methods("GET", "POST")
+
+	// Sub routers
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/", infoOfV1)
 	http.ListenAndServe(":8000", r)
 }
