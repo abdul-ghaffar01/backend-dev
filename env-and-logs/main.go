ef9722ef13c02d98b60825e961e9b9a97f3505cb
Abdul Ghaffar <agscontact777@gmail.com>
2026-02-07 20:23:34 +0500

    Done with structured logging

--- env-and-logs/main.go
+++ env-and-logs/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
 
-
-func main(){
+func main() {
 	fmt.Println("Started structured logging")
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	slog.SetDefault(logger)
+
+	slog.Info("Started server", "port", 8080)
+
+
+	logger = slog.With(
+		"Service", "auth",
+		"version", "1.0.0")
+	logger.Info("login successfull", "user_id", 13423)
+	logger.Error("Token not found")
+
 }
